bb3e00a2519aaa97fc0f81236bcf0241784abd5f
MiG27K <svatoslavn142@gmail.com>
2025-04-07 19:24:20 +0300

    Add files via upload

--- Task 27-01.py
+++ Task 27-01.py
@@ -1,57 +1,59 @@
 #Task 27-01
-file = open('27var1A.csv')
-lst = []
-for s in file:
-    #print(s)
-    pair = s.split(';')
-    pair[0] = float(pair[0].replace(',', '.'))
-    pair[1] = float(pair[1][:-1].replace(',', '.'))
-    #print(pair)
-    lst.append(pair)
+import turtle as t
 
-import math
+#t.tracer(0)
+#t.done()
+#t.lt(90)
+#k = 10
 
-def distance(P1, P2):
-    return math.sqrt((P2[0] - P1[0])**2 + (P2[1] - P1[1])**2)
+from math import *
 
-N = len(lst)
-labels = [0 for i in range(N)]
+file = [i[:-1].split(';') for i in open('27var1A.csv')]
+data = []
+for j in file:
+    data1 = []
+    for i in range(len(j)-1):
+        #эти числа надо как-то интовать
+        num1 = float(j[i].replace(',', '.'))
+        num2 = float(j[i + 1].replace(',', '.'))
+        print(num1, num2)
+        data.append([num1, num2])
+#print(data)
 
-def update_clusters(arr, labels, C1, C2):
-    N = len(arr)
-    for i in range(N):
-        P = arr[i]
-        distance_1 = distance(P, C1)
-        distance_2 = distance(P, C2)
-        if distance_1 < distance_2:
-            labels[i] = 0
-        else:
-            labels[i] = 1
-        return labels
+#t.up()
+#for p in file:
+ #   t.setpos(p[0] * k, p[1] * k)
+  #  t.dot(2)
 
-def find_centers(arr, labels):
-    x0, y0, x1, y1 = 0, 0, 0, 0
-    count0 = 0
-    count1 = 0
-    for i in range(N):
-        if labels[i] == 0:
-            count0 += 1
-            x0 += arr[i][0]
-            y0 += arr[i][1]
-        else:
-            count1 += 1
-            x1 += arr[i][0]
-            y1 += arr[i][1]
-    C0 = [x0 / count0, y0 / count0]
-    C1 = [x1 / count1, y1 / count1]
-    return C1, C0
+def clusterization(data):
+    eps = 1.5
+    clusters = []
+    while data:
+        el = data.pop()
+        clusters.append([el])
+        for p in clusters[-1]:
+            neighbours = [pt for pt in data if dist(p, pt) < eps]
+            clusters[-1] += neighbours
+            for pt in neighbours:
+                data.remove(pt)
+    return clusters
 
-def clusterization(arr, labels, max_iter):
-    C0 = [10, 10]
-    C1 = [0, 0]
-    for i in range(max_iter):
-        labels = update_clusters(arr, labels, C0, C1)
-        C0, C1 = find_centers(arr, labels)
-    return labels
+def centers(clusters):
+    cent = []
+    for cl in clusters:
+        d_min = 1000000
+        c = [0, 0]
+        for p in cl:
+            d = sum([dist(p, pt) for pt in cl])
+            if d < d_min:
+                d_min = d
+                c = p
+        cent.append(c)
+    return cent
 
-print(clusterization(lst, labels, 50))
+#print(centers(clusterization(data)))
+clusters = clusterization(data)
+cent = centers(clusters)
+Px = int(abs(sum(p[0] for p in cent) / len(cent)) * 10000)
+Py = int(abs(sum(p[1] for p in cent) / len(cent)) * 10000)
+print(Px, Py)
